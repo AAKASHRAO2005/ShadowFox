169d99ed8271bbc4243d0d59145f9fda476b418f
Akash <aakashrao794@gmail.com>
2026-01-31 02:38:58 +0530

    Delete data.js

--- data.js
+++
@@ -1,6 +0,0 @@
-const products = [
-  { id: 1, name: "Nike Air Max", brand: "Nike", price: 2999, image: "download.jpg" },
-  { id: 2, name: "Adidas Runner", brand: "Adidas", price: 1999, image: "download (1).jpg" },
-  { id: 3, name: "Puma Sports", brand: "Puma", price: 999, image: "download (3).jpg" },
-  { id: 4, name: "Nike ZoomX", brand: "Nike", price: 2599, image: "download (2).jpg" },
-];
